d7732307692f194779a7cbeeb9c377d8526be5dc
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-02-28 18:13:42 +0530

    - t

--- routers/report.js
+++ routers/report.js
@@ -69,31 +69,31 @@ reportRouter.put("/worker/update", workerPostAuth,async(req,res)=>{
     }
 })
 
-reportRouter.put("/assign",async(req,res)=>{
-    try {
-        const {id} = req.query
-        const report = await Report.findById(id)
-        if(!report)
-            res.status(404).json({message: "Error: report not found"})
-        const update = await Report.updateOne({_id: id},req.body )
-        res.status(200).json({update})
-    } catch (error) {
-        res.status(400).json({message: "Error: "+error})
-    }
-})
+// reportRouter.put("/assign",async(req,res)=>{
+//     try {
+//         const {id} = req.query
+//         const report = await Report.findById(id)
+//         if(!report)
+//             res.status(404).json({message: "Error: report not found"})
+//         const update = await Report.updateOne({_id: id},req.body )
+//         res.status(200).json({update})
+//     } catch (error) {
+//         res.status(400).json({message: "Error: "+error})
+//     }
+// })
 
-reportRouter.delete("/delete",async(req,res)=>{
-    try {
-        const {id} = req.query
-        const report = await Report.findById(id)
-        if(!report)
-            res.status(404).json({message: "Error: report not found"})
-        await Report.deleteOne({_id:id})
-        res.status(200).json({message: "delete sucessfull"})
-    } catch (error) {
-        res.status(400).json({message: "Error: "+error})
-    }
-})
+// reportRouter.delete("/delete",async(req,res)=>{
+//     try {
+//         const {id} = req.query
+//         const report = await Report.findById(id)
+//         if(!report)
+//             res.status(404).json({message: "Error: report not found"})
+//         await Report.deleteOne({_id:id})
+//         res.status(200).json({message: "delete sucessfull"})
+//     } catch (error) {
+//         res.status(400).json({message: "Error: "+error})
+//     }
+// })
 
 export default reportRouter
 
