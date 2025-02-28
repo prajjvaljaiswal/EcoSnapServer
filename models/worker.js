54f72377740d190f82a16227c0abdb0f4b66cfe1
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-03-01 03:36:39 +0530

    -worker

--- models/worker.js
+++ models/worker.js
@@ -8,6 +8,11 @@ const WorkerSchema = new mongoose.Schema({
     points: { type: Number, default: 0 },
     reportsAssigned: [{ type: mongoose.Schema.Types.ObjectId, ref: "Report" ,default:""}],
     status: { type: String, enum: ["Available", "In Progress"], default: "Available" },
+    active: {
+        type: String,
+        enum: ["Online", "Offline"],
+        default: "Online"
+    }
 }, { timestamps: true });
 const Worker = mongoose.model("Worker", WorkerSchema);
 export default Worker
