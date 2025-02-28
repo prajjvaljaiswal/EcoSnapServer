d9aad34cadcc640d1adccb997861a67142a13d2c
PRAJJVAL JAISWAL <prajjaiswal158@gmail.com>
2025-02-28 10:36:02 +0530

    models initialize

---
+++ models/admin.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+
+const AdminSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true } // Hashed
+}, { timestamps: true });
+const Admin = mongoose.model("Admin", AdminSchema)
+module.exports = Admin;
---
+++ models/user.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true }, // Hashed password
+    phone: { type: String , default:""},
+    location: { type: String , default:""}, // Optional user location
+    points: { type: Number, default: 0 }, // Reward points
+    reports: [{ type: mongoose.Schema.Types.ObjectId, ref: "Report" , default:""}], // Reports submitted
+    role: { type: String, enum: ["user", "admin"], default: "user" },
+}, { timestamps: true });
+
+UserSchema.statics.getUsersSortedByPoints = async function () {
+    return await this.find().sort({ points: -1 }); // Sort in descending order
+  };
+
+const User = mongoose.model("User", UserSchema)
+module.exports = User;
